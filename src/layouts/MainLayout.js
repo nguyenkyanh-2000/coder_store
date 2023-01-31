9956309b81c8f35d560a1bb30dbefe4908addf32
Anh Ky Nguyen <anh163@gmail.com>
2023-01-31 10:56:14 +0700

    FIx "Clear" Button

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -5,7 +5,7 @@ import MainHeader from "./MainHeader";
 
 function MainLayout() {
   return (
-    <Stack sx={{ minHeight: "100vh" }}>
+    <Stack sx={{ minHeight: "100vh", minWidth: "100vw" }}>
       <MainHeader />
 
       <Outlet />
